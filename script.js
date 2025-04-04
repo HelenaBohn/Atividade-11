863e580c0e30b31cc5fd1a03d7cd0dced7844803
HelenaBohn <helena_b_martins@estudante.sesisenai.org.br>
2025-04-04 12:31:13 -0300

    .

--- script.js
+++ script.js
@@ -4,4 +4,29 @@ function print(msg) {
     console.log(msg)
 }
 
-//questão1
+//questão 1-Somador de Números 
+
+//questão 2-Contagem Regressiva da NASA  
+let contagem = 10;
+let intervalo = setInterval(() => {
+    if (contagem >= 0) {
+        print(contagem);
+        contagem--;
+    } else {
+        clearInterval(intervalo);
+        print("Lançamento!");
+    }
+}, 1000);
+
+//questão 9-Contador de Vogais  
+let frase = prompt("Digite uma frase:");
+let contador = 0;
+let vogais = "aeiouAEIOU"; 
+
+for (let i = 0; i < frase.length; i++) {
+    if (vogais.includes(frase[i])) { 
+        contador++;
+    }
+}
+
+alert(`A frase digitada tem ${contador} vogais.`);
